1ce6b9e00f92d6bb0a056155812d3eac74ab751e
tqace <1260895506@qq.com>
2020-10-21 18:07:56 +0800

    ,

--- pretrain.sh
+++ pretrain.sh
@@ -1,1 +1,1 @@
-./scripts/dist_run_single.sh 1 pretrain/train_end2end.py cfgs/pretrain/base_e2e_16x16G_fp16.yaml visual_embedding
+./scripts/dist_run_single.sh 4 pretrain/train_end2end.py cfgs/pretrain/base_e2e_16x16G_fp16.yaml visual_embedding
